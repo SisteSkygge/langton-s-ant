bb4287e2cec3422d1850239db84aaf894c567585
blacktime <lordigna@gmail.com>
2018-06-20 20:06:14 +0200

    separation de game en 2 fichiers game.js et gameClient.js un pour le serveur et un plus leger pour le client

---
+++ public/gameClient.js
@@ -0,0 +1,19 @@
+class Game{
+    constructor(height, width){
+        this.height = height;
+        this.width = width;
+        this.blackPixel = [];
+    }
+
+    ajouterPixel(x, y){
+        blackPixel.push(x+y*width);
+    }
+
+    loadMap(data){
+        /*
+            Charge la carte envoyé par le serveur
+        
+        */
+       this.blackPixel.push("nothing");
+    }
+}
