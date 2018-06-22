88b4761302632cdefbfc9a905e71c5548bf06fa7
blacktime <lordigna@gmail.com>
2018-06-22 07:52:33 +0200

    test

--- test.py
+++ test.py
@@ -1,9 +1,15 @@
-from numpy import zeros
+import numpy
 import zlib
-a = zeros((640,640), dtype=bool)
+a = numpy.zeros((640,640), dtype=bool)
 b = a.tobytes()
 c = zlib.compress(b)
 
 with open('dump.bin', 'wb') as file:
     file.write(c)
+    file.close()
+
+with open('dump.bin', 'rb') as file:
+    b = zlib.decompress(file.readline())
+    a = numpy.fromstring(b, bool)
+    print(a[0:10])
     file.close()
